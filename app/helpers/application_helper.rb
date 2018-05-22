fbab98633085b17166f2b0fdfea99223e48a5ac1
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-21 23:23:17 -0400

    add security helper for private chat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
         opts[:alt] = user.name
         image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{opts.delete(:size) { 40 }}", opts
     end
+
+    def can_see?(private_chat_room)
+        current_user.id == private_chat_room.user_id || current_user.id == private_chat_room.other_user_id
+    end
 end
